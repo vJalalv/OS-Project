a0ac18fdf23b72e1aeabaa9c708ba68c9a84049f
vJalalv <galalramadan146@gmail.com>
2026-04-23 20:35:03 +0200

    Add header file for SRTF scheduling functions

---
+++ src/srtf.h
@@ -0,0 +1,9 @@
+#ifndef SRTF_H
+#define SRTF_H
+
+#include "process.h"
+
+void srtf_scheduling(Process *processes, int n);
+void print_gantt_srtf(int *gantt, int *gantt_time, int gantt_size);
+
+#endif
